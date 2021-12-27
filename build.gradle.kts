b56971389f04a1b1a6a84920e1390c5d2b3f658a
DragonKnigtOfBreeze <dk_breeze@qq.com>
2021-12-27 15:14:31 +0800

    修复动态模版上下文判定的bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "icu.windea"
-version = "3.2.0"
+version = "3.2.1"
 
 intellij {
 	version = "2021.3"
--- src/main/kotlin/icu/windea/starboundText/editor/StarboundTextTemplateContext.kt
+++ src/main/kotlin/icu/windea/starboundText/editor/StarboundTextTemplateContext.kt
@@ -1,14 +1,15 @@
 package icu.windea.starboundText.editor
 
-import com.intellij.codeInsight.template.*
-import com.intellij.psi.*
+import com.intellij.codeInsight.template.TemplateContextType
+import com.intellij.psi.PsiFile
 import icu.windea.starboundText.*
-import icu.windea.starboundText.psi.*
+import icu.windea.starboundText.psi.StarboundTextFile
+import icu.windea.starboundText.psi.StarboundTextString
 
-class StarboundTextTemplateContext : TemplateContextType(starboundTextId, starboundTextName){
-	override fun isInContext(file: PsiFile, offset: Int): Boolean {
-		val element = file.findElementAt(offset)?:return false
-		val parent = element.parent?:return false
-		return parent is PsiFile || parent is StarboundTextString
-	}
+class StarboundTextTemplateContext : TemplateContextType(starboundTextId, starboundTextName) {
+    override fun isInContext(file: PsiFile, offset: Int): Boolean {
+        val element = file.findElementAt(offset) ?: return false
+        val parent = element.parent ?: return false
+        return parent is StarboundTextString
+    }
 }
